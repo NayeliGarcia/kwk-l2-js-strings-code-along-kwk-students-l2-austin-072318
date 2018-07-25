5c7f8e778052eef3f384a8c477a1a273889850ee
NayeliGarcia <nayeli.garcia2222@gmail.com>
2018-07-25 15:26:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ let currentUser = "Nayeli Garcia"
 let welcomeMessage ="Welcome to Klossybook, "
 welcomeMessage = welcomeMessage + currentUser + "!"
 let excitedWelcomeMessage= welcomeMessage.toUpperCase() 
+let shortGreeting = "Welcome, " + currentUser 
